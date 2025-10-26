c7524854cb985785a60be94f692c347a7a162735
Nguyen Dang Khoa <khoa36015@gmail.com>
2025-10-26 14:44:23 +0700

    adding index to province

--- backend/generate-information.py
+++ backend/generate-information.py
@@ -2,47 +2,42 @@ from flask import Flask, jsonify
 import requests
 from flask_cors import CORS
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
-
 app = Flask(__name__)
 CORS(app)
-
+# them cors header
 def add_cors_headers(resp):
-    resp.headers["Access-Control-Allow-Origin"] = "*" 
+    resp.headers["Access-Control-Allow-Origin"] = "*"
     resp.headers["Vary"] = "Origin"
     resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     resp.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return resp
-
-
+# tao dieu huong cho api
 @app.route('/api/ai/get-information-mientay', methods=['GET'])
 def info_mientay():
     try:
+        # goi api goc
         mientay_api = 'http://localhost:8000/api/ai/agent'
         payload = {'message': 'generate 6 tỉnh miền tây nổi bật random'}
-
-       
         r = requests.post(mientay_api, json=payload, timeout=10)
-        r.raise_for_status() 
-
-        data = r.json() 
-
-        response = jsonify({
-            "status": "success",
-            "source": mientay_api,
-            "data": data
-        })
-        return add_cors_headers(response)
-
+        r.raise_for_status()
+        data = r.json()
+        # loc du lieu va chuyen du lieu ve array sau do chuyen nguoc lai json
+        tinh = (data.get("reply") or {}).get("tinh", [])
+        if not isinstance(tinh, list):
+            tinh = []
+        MEKONG_PROVINCES = [
+            "an giang", "bac lieu", "ben tre", "ca mau", "can tho", "dong thap",
+            "hau giang", "kien giang", "long an", "soc trang", "tien giang", "tra vinh",
+            "vinh long"]
+        result = []
+        for i, tinh in enumerate(MEKONG_PROVINCES, start=1):
+            if tinh in MEKONG_PROVINCES:
+                result.append(i)
+        return add_cors_headers(jsonify(result)), 200
     except requests.exceptions.RequestException as e:
-
-        response = jsonify({
-            "status": "error",
-            "error": str(e)
-        })
-        return add_cors_headers(response), 502
-
-
+        return add_cors_headers(jsonify({"status": "error", "error": str(e)})), 502
 if __name__ == '__main__':
     app.run(port=40, debug=True)
