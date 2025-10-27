fea146334be5140c4db14302e5aa67423fce1021
Nguyen Dang Khoa <khoa36015@gmail.com>
2025-10-27 13:57:00 +0700

    test

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,16 +1,19 @@
- !DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="inline-block header">
-        <a class="logo w-5">
-            <h1>Mien Tay Song Nuoc</h1>
-        </a>
-        <div class="button-nav">
-            <button class=""></button>
+    <div class="container">
+        <div class="header inline-block fixed">
+            <div class="header-logo"><img src="#" class = ""></div>
+            <div class="button inline-block relative">
+                <button class="menu-button">Menu</button>
+                <button class="menu-button">Menu</button>
+                <button class="menu-button">Menu</button>
+                
+            </div>
         </div>
     </div>
 </body>
